429decd43659ccad1dc7fd9c7b7586bf28b21b96
jungyu <jungyu4281@gmail.com>
2025-02-21 11:55:20 +0900

    auth - user,employer login, signup end
    auth - user,employer login, signup end sechema update

--- data/auth.js
+++ data/auth.js
@@ -1,21 +1,6 @@
 import User from "../models/userschema.js";
 import Employer from "../models/employerschema.js";
 
-// 일반 사용자 이메일로 조회
-export async function findByEmail(email) {
-  return await User.findOne({ email });
-}
-
-// 고용주 이메일로 조회
-export async function findEmployerByEmail(email) {
-  return await Employer.findOne({ email });
-}
-
-// 전화번호로 유저 찾기
-export async function findByPhone(phone_number) {
-  return await User.findOne({ "phone.number": phone_number });
-}
-
 // 사용자 회원가입
 export async function createUser(userData) {
   const newUser = new User(userData);
@@ -28,24 +13,19 @@ export async function createEmployer(employerData) {
   return await newEmployer.save();
 }
 
-// ID로 유저 찾기
-export async function findById(id) {
-  return await User.findById(id);
+// 일반 사용자 이메일로 조회
+export async function findByEmail(email) {
+  return await User.findOne({ email });
 }
 
-// ID로 고용주 찾기
-export async function findEmployerById(id) {
-  return await Employer.findById(id);
+// 고용주 이메일로 조회
+export async function findEmployerByBusinessNumber(businessNumber) {
+  return await Employer.findOne({ businessNumber });
 }
 
-// 회원 탈퇴 (일반 사용자)
-export async function deleteUserById(id) {
-  return await User.findByIdAndDelete(id);
-}
-
-// 회원 탈퇴 (고용주)
-export async function deleteEmployerById(id) {
-  return await Employer.findByIdAndDelete(id);
+// 전화번호로 유저 찾기
+export async function findByPhoneUser(phone_number) {
+  return await User.findOne({ "phone.number": phone_number });
 }
 
 // 유저 정보 기반 이메일 찾기
